a37f5f3178f97631e03de1a82505f07f98dd733d
Carlos Macario <cmacariovx@gmail.com>
2022-12-26 06:35:58 -0500

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import './App.css'
 import Landing from './Pages/Landing'
 import Home from './Pages/Home'
 import Profile from './Pages/Profile'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/' exact element={<Landing />} />
         <Route path='/home' exact element={<Home />} />
         <Route path='/profile/:username' exact element={<Profile />} /> {/*path will be username*/}
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/signup' exact element={<Signup />} />
       </Routes>
     </div>
   )
---
+++ client/src/Components/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="loginBackdrop">
+            <div className="loginContainer">
+                <div className="loginHeaderContainer">
+                    <p className="loginHeaderText">Login</p>
+                </div>
+                <div className="loginBodyContainer">
+                    <div className="loginInputContainer">
+                        <p className="loginUsernameText">Username</p>
+                        <input className="loginInput" id="loginInput1"></input>
+                    </div>
+                    <div className="loginInputContainer">
+                        <p className="loginPasswordText">Password</p>
+                        <input type="password" className="loginInput" id="loginInput3"></input>
+                    </div>
+                    <div className="loginButtonsContainer">
+                        <Link to="/home">
+                            <button className="loginButton">Login</button>
+                        </Link>
+                        <Link to="/home">
+                            <button className="demoLoginButton">Demo Login</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="loginFooterContainer">
+                    <p className="loginFooterText">Don't Have an Account?</p>
+                    <Link className="loginFooterSignupLink" to="/signup">Sign Up</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
