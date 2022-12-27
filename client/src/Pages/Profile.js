d82f1563bc3f019426e29afffb44fe678b7e70eb
Carlos Macario <cmacariovx@gmail.com>
2022-12-27 11:07:07 -0500

    fixed profile page bug

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -9,6 +9,7 @@ import HomeDetailedPostCard from "../Components/UI/HomeDetailedPostCard"
 
 function Profile() {
     let [cardClick2, setCardClick2] = useState(false)
+    let [detailedCardData2, setDetailedCardData2] = useState({})
 
     function detailedCard2() {
         setCardClick2(true)
@@ -16,6 +17,30 @@ function Profile() {
 
     function closeCard2() {
         setCardClick2(false)
+    }
+
+    let mainPostFeedPosts2 = [
+        {
+            'username': '@cmacariovx',
+            'mainBodyText': 'Warzone 2 is looking a bit more polished now!',
+            'likeCount': 543,
+        },
+        {
+            'username': '@cmacariovx',
+            'mainBodyText': 'Testing',
+            'likeCount': 2,
+        },
+        {
+            'username': '@cmacariovx',
+            'mainBodyText': 'Does Socia have star potential?',
+            'likeCount': 991,
+        }
+    ]
+
+    let [listOfPosts2, setListOfPosts2] = useState(mainPostFeedPosts2)
+
+    function detailedCardDataHandler2(detailedPostData2) {
+        setDetailedCardData2(detailedPostData2)
     }
 
     return (
@@ -47,8 +72,8 @@ function Profile() {
                     <button className="editButton">Edit Profile</button>
                 </div>
                 <div className="mainProfilePostsFeedContainer">
-                    {cardClick2 && <HomeDetailedPostCard onCloseCard={closeCard2}/>}
-                    <HomePostCard onShowCard={detailedCard2}/>
+                    {cardClick2 && <HomeDetailedPostCard onCloseCard={closeCard2} detailedCardData={detailedCardData2}/>}
+                    {listOfPosts2.map((post, index) => <HomePostCard onShowCard={detailedCard2} homePostCardData={post} onDetailedCardDataHandler={detailedCardDataHandler2} key={index}/>)}
                 </div>
             </div>
         </div>
