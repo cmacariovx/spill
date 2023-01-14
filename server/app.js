d45340be0178a26761b5406e40ec6fac157adba4
Carlos Macario <cmacariovx@gmail.com>
2023-01-14 13:35:06 -0500

    cors

--- server/app.js
+++ server/app.js
@@ -7,10 +7,30 @@ const mainRouter = require('./routes/mainRoutes')
 const app = express()
 
 // app.use("/", mainRouter)
+
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+
+    next()
+})
 
 app.post("/users", mongoPractice.mainFetch)
 
 app.get("/users", mongoPractice.mainGet)
 
+app.use((req, res, next) => {
+    const error = new Error("Could not find this route.")
+    throw error
+})
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) return next(error)
+
+    res.status(error.code || 500)
+    res.json({"message": error.message || "Unknown error occured."})
+})
 app.listen(5000)
