546004256188b09283a3b8b6dd036a8fb4af469b
Carlos Macario <cmacariovx@gmail.com>
2023-02-20 16:04:18 -0500

    aws s3 file storage

--- server/middleware/file-upload.js
+++ server/middleware/file-upload.js
@@ -1,29 +1,60 @@
 const multer = require('multer')
 const uuid = require('uuid')
 
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
+require("dotenv").config()
+
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'us-east-2'
+});
+
+const s3 = new aws.S3()
+
 const MIME_TYPE_MAP = {
     'image/png': 'png',
     'image/jpg': 'jpg',
     'image/jpeg': 'jpeg'
 }
 
-const fileUpload = multer({
-    limits: 500000,
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, 'uploads/images')
-        },
-        filename: (req, file, cb) => {
-            const ext = MIME_TYPE_MAP[file.mimetype]
-            cb(null, uuid.v1() + '.' + ext)
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: 'public-read',
+        bucket: 'spillimages',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, Date.now().toString()); //use Date.now() for unique file keys
         }
     }),
     fileFilter: (req, file, cb) => {
         const isValid = !!MIME_TYPE_MAP[file.mimetype]
         let error = isValid ? null : new Error('Invalid mime type')
-        
+
         cb(error, isValid)
-    }
-})
+    },
+    limits: 500000,
+});
+
+// const fileUpload = multer({
+//     limits: 500000,
+//     storage: multer.diskStorage({
+//         destination: (req, file, cb) => {
+//             cb(null, 'uploads/images')
+//         },
+//         filename: (req, file, cb) => {
+//             const ext = MIME_TYPE_MAP[file.mimetype]
+//             cb(null, uuid.v1() + '.' + ext)
+//         }
+//     }),
+//     fileFilter: (req, file, cb) => {
+//         const isValid = !!MIME_TYPE_MAP[file.mimetype]
+//         let error = isValid ? null : new Error('Invalid mime type')
+
+//         cb(error, isValid)
+//     }
+// })
 
 module.exports = fileUpload
