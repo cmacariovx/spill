78330a09da2830fbac0bcebb9f687b70179a7e72
Carlos Macario <cmacariovx@gmail.com>
2023-02-20 16:56:31 -0500

    small change

--- server/middleware/file-upload.js
+++ server/middleware/file-upload.js
@@ -31,7 +31,7 @@ const fileUpload = multer({
     }),
     fileFilter: (req, file, cb) => {
         const isValid = !!MIME_TYPE_MAP[file.mimetype]
-        let error = isValid ? null : new Error('Invalid mime type')
+        let error = isValid ? null : JSON.stringify('Invalid mime type')
 
         cb(error, isValid)
     },
